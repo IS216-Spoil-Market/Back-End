2ee1e4e7ea4e9df99803c0a2e7634da9a5dbf10d
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-25 21:13:30 +0800

    Create auth service to decode JWT and fetch user info from Auth0

---
+++ src/service/authService.js
@@ -0,0 +1,57 @@
+const dotenv = require("dotenv");
+const { auth } = require("express-oauth2-jwt-bearer");
+const axios = require("axios");
+
+dotenv.config();
+
+/**
+ * Type: Middleware
+ *
+ * Purpose: Validate JWT Token to protect routes
+ *
+ * @returns {e.Handler}
+ */
+const checkJwt = (req, res, next) =>
+    auth({
+        audience: `${req.protocol}://${req.hostname}:5001`,
+        issuerBaseURL: process.env.AUTH0_DOMAIN,
+    })(req, res, next);
+
+/**
+ * Type: Middleware
+ *
+ * Purpose: Fetch user info based on token from Auth0
+ *
+ * Pre-requisite: Must call checkJwt middleware first
+ *
+ * @returns {Promise<void>}
+ */
+const fetchUserInfo = async (req, res, next) => {
+    try {
+        const { data } = await axios.get(
+            `${process.env.AUTH0_DOMAIN}/userinfo`,
+            {
+                headers: {
+                    Authorization: `Bearer ${req.auth.token}`,
+                },
+            }
+        );
+
+        req.user = data;
+
+        next();
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            code: 500,
+            error: "Internal Server Error",
+            message: "Unable to retrieve user info from Auth0",
+        });
+    }
+};
+
+module.exports = {
+    checkJwt,
+    fetchUserInfo,
+};
