aca693c94fec1b2c31c876b4145086afc9aa260a
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-27 17:01:27 +0800

    Add update profile request url to profile controller

--- src/controller/profile.js
+++ src/controller/profile.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { fetchUserInfo } = require("../service/authService");
-const { createOrGetUser } = require("../model/user");
+const { createOrGetUser, updateProfile } = require("../model/user");
 
 router.post("/", fetchUserInfo, async (req, res) => {
     const [code, response] = await createOrGetUser(req, res);
     res.status(code).json(code === 500 ? { code, ...response } : response);
+});
+
+router.put("/", fetchUserInfo, async (req, res) => {
+    const [code, response] = await updateProfile(req, res);
+    res.status(code).json(code === 200 ? response : { code, ...response });
 });
 
 module.exports = router;
