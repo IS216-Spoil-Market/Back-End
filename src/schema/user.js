49f2c772791ddab9e64ceb5e9822d3613ae82088
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-11-01 22:07:32 +0800

    Modify user schema to include timestamps

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,36 +1,39 @@
 const mongoose = require("mongoose");
 const muv = require("mongoose-unique-validator");
 
-const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        require: true,
-        unique: true,
+const UserSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            require: true,
+        },
+        picture: {
+            type: String,
+            require: true,
+        },
+        about: {
+            type: String,
+            require: true,
+            default: "",
+        },
+        skills_interested: {
+            type: [String],
+            require: true,
+            default: [],
+        },
+        my_skills: {
+            type: [String],
+            require: true,
+            default: [],
+        },
     },
-    name: {
-        type: String,
-        require: true,
-    },
-    picture: {
-        type: String,
-        require: true,
-    },
-    about: {
-        type: String,
-        require: true,
-        default: "",
-    },
-    skills_interested: {
-        type: [String],
-        require: true,
-        default: [],
-    },
-    my_skills: {
-        type: [String],
-        require: true,
-        default: [],
-    },
-});
+    { timestamps: true }
+);
 
 UserSchema.set("toJSON", {
     // This is to return _id as id and the unnecessary _v property
